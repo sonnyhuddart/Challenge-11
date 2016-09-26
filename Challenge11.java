7b3ec775287e899d873a51f73e851c4c77fa3a5c
sonnyhuddart <sonny.huddart@live.co.uk>
2016-09-26 14:32:05 +0100

    Add files via upload

---
+++ Challenge11.java
@@ -0,0 +1,16 @@
+package challenge.pkg11;
+import java.util.Scanner;
+public class Challenge11 {
+
+   
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num1 = myScanner.nextInt();
+        for(int i = 0; i <= num1; i++) {
+         System.out.println(i);
+       
+    }
+    }    
+    
+}
